61bf08bfd37e6d565897e27416bb3c4a7242e689
Joshua Revord <jarevord@mail.fhsu.edu>
2021-05-10 22:26:57 -0500

    heroku db connection

--- config/Database.php
+++ config/Database.php
@@ -3,15 +3,16 @@
 	class Database {
 		//DB Params
 
-		private $dsn = 'mysql:host=localhost;dbname=quotesdb';
-		private $username = 'root';
+		private $dsn = 'mysql:host=y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ebgthxuzb8tnkfwr';
+		private $username = 'ydvbdiox0kk7kzdm';
+		private $password = 'x72oldqt3rvcj2yb'
 		private $db;
 
 		public function connect() {
 			$this->db = null;
 
 			try {
-				$this->db = new PDO($this->dsn, $this->username);
+				$this->db = new PDO($this->dsn, $this->username, $this->password);
 				$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			}catch(PDOException $e){
 				echo 'Connection Error: ' . $e->getMessage();
